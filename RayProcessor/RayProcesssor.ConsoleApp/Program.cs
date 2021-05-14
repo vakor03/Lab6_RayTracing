f10030e9fdb3d3df5c9b39f407e0b598e85af0f3
Sviatozar <svpetrenko123@gmail.com>
2021-05-14 22:34:10 +0300

    Program

--- RayProcessor/RayProcesssor.ConsoleApp/Program.cs
+++ RayProcessor/RayProcesssor.ConsoleApp/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using RayProcesssor.Lib;
+using RayProcessor.Lib;
 
 namespace RayProcesssor.ConsoleApp
 {
@@ -7,8 +7,10 @@ namespace RayProcesssor.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Screen screen = new(50, 100, (1, 1), new(1, 1, 0), new(0, 0, 0));
-            Console.WriteLine(screen.GetPixelXYZPoint(50, 100));
+            //Screen screen = new(50, 100, (1, 1), new(1, 1, 0), new(0, 0, 0));
+            Sphere sphere = new(new(0, 100, 0), 50);
+            Ray ray = new(new(1, 0, 0), new(0, 0, 0));
+            Console.WriteLine(sphere.RayCrossesSphere(ray));
         }
     }
 }
