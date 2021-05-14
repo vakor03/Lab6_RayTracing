87cff82466f9824bdeece38e544bc9313ae4ee5b
Sviatozar <svpetrenko123@gmail.com>
2021-05-14 20:51:48 +0300

    Add screen class

---
+++ RayProcessor/RayProcesssor.ConsoleApp/Screen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayProcesssor.ConsoleApp
+{
+    class Screen
+    {
+        // colors of each pixel
+        char[,] pixels;
+
+        public Screen(int width, int height)
+        {
+            pixels = new char[width, height];
+        }
+    }
+}
