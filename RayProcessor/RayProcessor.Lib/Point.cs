bdb9ddb5ece6280026dcbaff459f6775a8af5453
Sviatozar <svpetrenko123@gmail.com>
2021-05-14 21:38:57 +0300

    Add helper methods for Point

--- RayProcessor/RayProcessor.Lib/Point.cs
+++ RayProcessor/RayProcessor.Lib/Point.cs
@@ -8,15 +8,33 @@ namespace RayProcesssor.Lib
 {
     struct Point
     {
-        int x;
-        int y;
-        int z;
+        double x;
+        double y;
+        double z;
 
-        public Point(int x, int y, int z)
+        double magnitude;
+
+        public Point(double x, double y, double z)
         {
             this.x = x;
             this.y = y;
             this.z = z;
+
+            magnitude = Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public Point GetDirectionalСosinuses()
+        {
+
+            return new Point(x / magnitude, y / magnitude, z / magnitude);
+        }
+
+        public Point GetDirectionalSinuses()
+        {
+            Point directionalCosinuses = GetDirectionalSinuses();
+            return new Point(Math.Sqrt(1-Math.Pow(directionalCosinuses.x, 2)),
+                Math.Sqrt(1 - Math.Pow(directionalCosinuses.y, 2)),
+                Math.Sqrt(1 - Math.Pow(directionalCosinuses.z, 2)));
         }
     }
 }
