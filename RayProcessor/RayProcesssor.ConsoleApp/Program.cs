d2616c341daf614d75160fc45c01beb4e2a4c33a
Valerii_kor <myksv200@gmail.com>
2021-05-15 00:01:28 +0400

    TestFace

--- RayProcessor/RayProcesssor.ConsoleApp/Program.cs
+++ RayProcessor/RayProcesssor.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace RayProcesssor.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Face testFace = new Face(new[] {new Point(0, 0, 0), new Point(0, 2, 0), new Point(2, 0, 0)});
             
         }
     }
