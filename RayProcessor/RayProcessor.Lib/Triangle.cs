abf345fbf7ce0c1d037ef387bec0003e26a867d0
Marichka <marichka.v.richtsi@gmail.com>
2021-05-15 14:09:03 +0300

    Add method isCrossesTriangle

--- RayProcessor/RayProcessor.Lib/Triangle.cs
+++ RayProcessor/RayProcessor.Lib/Triangle.cs
@@ -1,4 +1,7 @@
-﻿namespace RayProcessor.Lib
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RayProcessor.Lib
 {
     public class Triangle
     {
@@ -16,6 +19,37 @@
 
         public bool IsCrossesTriangle(Ray ray)
         {
+            Point e1 = vertex1 - vertex2;
+            Point e2 = vertex1 - vertex3;
+
+            Normal = CrossProduct(e1, e2);
+
+            double tmp = DotProduct(Normal, ray.Vector); // хз як це назвати???
+            if (tmp < 0.000001) // промінь паралельний до трикутника
+            {
+                return false;
+            }
+
+            Point vectorTmp = ray.StartPoint - vertex1;
+            double t = -DotProduct(vectorTmp, Normal); //відстань від початку промення до точки перетину 
+            
+            // u i v це барицентричні координати
+            double u = VectorLenght(CrossProduct(e1, ray.Vector)) + VectorLenght(CrossProduct(e1, vectorTmp)); 
+            u /= VectorLenght(Normal);
+
+            if (u < 0 || u > 1)
+            {
+                return false;
+            }
+            
+            double v = VectorLenght(CrossProduct(e2, ray.Vector)) + VectorLenght(CrossProduct(e2, vectorTmp));
+            v /= VectorLenght(Normal);
+            
+            if (v < 0 || v > 1|| u+v>1)
+            {
+                return false;
+            }
+            
             return true;
         }
 
@@ -25,6 +59,16 @@
             double y = (-1)*(vector1.x * vector2.z - vector2.z * vector1.x);
             double z = vector1.x * vector2.y - vector2.y * vector1.x;
             return new Point(x, y, z);
+        }
+
+        private double DotProduct(Point vector1, Point vector2) //скалярний добуток
+        {
+            return vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z;
+        }
+
+        private double VectorLenght(Point vector)
+        {
+            return Math.Sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
         }
     }
 }
