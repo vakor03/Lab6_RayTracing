abd38d560335ea7b8800a4e7cf87887465d82df5
Marichka <marichka.v.richtsi@gmail.com>
2021-05-15 13:12:02 +0300

    Add operator +

--- RayProcessor/RayProcessor.Lib/Point.cs
+++ RayProcessor/RayProcessor.Lib/Point.cs
@@ -18,6 +18,11 @@ namespace RayProcessor.Lib
             magnitude = Math.Sqrt(x * x + y * y + z * z);
         }
 
+        public static Point operator -(Point p1, Point p2)
+        {
+            return new Point(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
+        }
+
         public Point GetDirectionalСosinuses()
         {
 
